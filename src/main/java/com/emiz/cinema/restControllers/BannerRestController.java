31d5e06290c121460285eacfbdcffefc4fd19ef1
emiz98 <64089619+emiz98@users.noreply.github.com>
2021-09-24 21:19:28 +0530

    Rest Controllers Cross Origin Fix

--- src/main/java/com/emiz/cinema/restControllers/BannerRestController.java
+++ src/main/java/com/emiz/cinema/restControllers/BannerRestController.java
@@ -2,12 +2,14 @@ package com.emiz.cinema.restControllers;
 
 import com.emiz.cinema.models.Banner;
 import com.emiz.cinema.services.BannerService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class BannerRestController {
--- src/main/java/com/emiz/cinema/restControllers/MovieRestController.java
+++ src/main/java/com/emiz/cinema/restControllers/MovieRestController.java
@@ -4,13 +4,11 @@ import com.emiz.cinema.models.Movie;
 import com.emiz.cinema.models.MovieShowTime;
 import com.emiz.cinema.services.MovieService;
 import com.emiz.cinema.services.MovieShowTimeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class MovieRestController {

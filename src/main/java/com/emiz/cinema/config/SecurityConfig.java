a442721eede07a954bf99257c55acdee02e4a862
emiz98 <64089619+emiz98@users.noreply.github.com>
2021-10-06 20:37:40 +0530

    api fix3 (permit:all)

--- src/main/java/com/emiz/cinema/config/SecurityConfig.java
+++ src/main/java/com/emiz/cinema/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.emiz.cinema.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -18,9 +21,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login").permitAll()
-//                .and()
-//                .authorizeRequests().antMatchers("/api/v1/**").permitAll()
                 .and()
                 .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/api/v1/**");
     }
 }

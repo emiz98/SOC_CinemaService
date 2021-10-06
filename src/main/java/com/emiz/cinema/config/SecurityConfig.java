ce0253da65d7b6b366ffddbdc0e371801418bce5
emiz98 <64089619+emiz98@users.noreply.github.com>
2021-10-06 19:58:33 +0530

    Dashboard Protection (spring security)

---
+++ src/main/java/com/emiz/cinema/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.emiz.cinema.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login");
+    }
+}
---
+++ src/main/java/com/emiz/cinema/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.emiz.cinema.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
